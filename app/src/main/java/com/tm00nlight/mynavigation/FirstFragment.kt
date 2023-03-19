2574ca43b676e4735df508487930bd2088505bf2
lukianets567 <71081602+lukianets567@users.noreply.github.com>
2023-03-19 13:57:04 +0200

    Initial commit

---
+++ app/src/main/java/com/tm00nlight/mynavigation/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.tm00nlight.mynavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.tm00nlight.mynavigation.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.root.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/tm00nlight/mynavigation/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.tm00nlight.mynavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.tm00nlight.mynavigation.databinding.FragmentFirstBinding
+import com.tm00nlight.mynavigation.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.root.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+    }
+}
